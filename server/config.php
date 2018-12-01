5af2395f8426e5e59e05104f59ec52a6186cc33f
ankit <ankit@qexon.com>
2018-12-01 13:00:20 +0530

    chnaged server credentials

--- server/config.php
+++ server/config.php
@@ -3,7 +3,7 @@
 	
 	$mysql_server = "139.59.86.83"; // mysql server
 	$mysql_user = "root"; // mysql user
-	$mysql_pass = "Purplemist"; //mysql password for user
+	$mysql_pass = "tinsink"; //mysql password for user
 	$mysql_db = "otrs"; // mysql database to use
 	
 	$conn = mysqli_connect($mysql_server,$mysql_user,$mysql_pass,$mysql_db);
